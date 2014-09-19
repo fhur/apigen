c5518bf42e93a0305c4de43c54720df55475fd25
fhur <fernando@authy.com>
2014-09-19 01:00:01 -0500

    made default type arg symbol insteadof str

--- lib/path_param.rb
+++ lib/path_param.rb
@@ -2,7 +2,7 @@ class PathParam
   attr_accessor :name
   attr_accessor :type
 
-  def initialize(name: force_name, type: 'string')
+  def initialize(name: force_name, type: :string)
     @name, @type = name, type
   end
 end
--- lib/query_param.rb
+++ lib/query_param.rb
@@ -3,7 +3,7 @@ class QueryParam
   attr_accessor :name
   attr_accessor :type
 
-  def initialize(name: force_name, type: 'string')
+  def initialize(name: force_name, type: :string)
     @name, @type = name, type
   end
 end

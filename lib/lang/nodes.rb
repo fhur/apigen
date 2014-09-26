ed0bfcfbf73fc6fca28a519418608608492bcbd5
fhur <fernando@authy.com>
2014-09-26 09:04:08 -0500

    Added helper methods to class Nodes to filter Node data

--- lib/lang/nodes.rb
+++ lib/lang/nodes.rb
@@ -1,4 +1,26 @@
-class Nodes < Struct.new :nodes; end
+class Nodes < Struct.new :nodes
+
+  def query_nodes
+    get_nodes type: QueryNode
+  end
+
+  def header_nodes
+    get_nodes type: HeaderNode
+  end
+
+  def path_nodes
+    get_nodes type: PathNode
+  end
+
+  def get_nodes(type:nil)
+    nodes[1].select { |i| i.first.is_a? type }
+  end
+
+  def url_method
+    nodes.first
+  end
+
+end
 
 class TypeNode < Struct.new :type, :required; end
 

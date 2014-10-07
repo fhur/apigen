3d5c0b2e679e2a1ccdec9df51fc0e1cb60b44775
fhur <fernando@authy.com>
2014-10-07 00:38:10 -0500

    Created specs for apigen_compiler

--- test/apigen_compiler_spec.rb
+++ test/apigen_compiler_spec.rb
@@ -12,16 +12,21 @@ describe ApigenCompiler do
 
     it "should compile simple endpoints" do
       code = """
-      get /users/{user_id}
-      @name get_user
+      # post /users/{user_id}/foo/bar/{baz}
+      # @name somename
+      # @path {string} user_id
+      # @path {boolean:optional} baz
       """
+
       endpoint_group = @compiler.compile code, "users"
       endpoint_group.name.must_equal "users"
       endpoints = endpoint_group.endpoints
       endpoints.size.must_equal 1
-      endpoints.first.method.must_equal HttpMethod.get
-      endpoints.first.url.must_equal "/users/{user_id}"
-      endpoints.first.name.must_equal "get_user"
+      endpoint = endpoints.first
+      endpoint.url.must_equal "/users/{user_id}/foo/bar/{baz}"
+      endpoint.name.must_equal "somename"
+      endpoint.path_params.size.must_equal 2
+      endpoint.path_params[:user_id].name.must_equal :user_id
 
     end
 

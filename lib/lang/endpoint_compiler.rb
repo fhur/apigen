cab727e3a1ccd5339b9da6d2e1d00b7813eace58
fhur <fernando@authy.com>
2014-10-07 00:37:50 -0500

    Simplified endpoint_compiler, now it only takes as input one single code and outputs the corresponding endpoint for that code

--- lib/lang/endpoint_compiler.rb
+++ lib/lang/endpoint_compiler.rb
@@ -12,33 +12,31 @@ class EndpointCompiler
     @parser = Parser.new
   end
 
-  def compile(*codes)
-    codes.map do |code|
-      nodes = @parser.parse(code)
-      url_method_node = nodes.url_method
-      method = HttpMethod.create(url_method_node.method.to_sym)
-      url = url_method_node.url
-      name = nodes.name_node.name
-      endpoint = Endpoint.new url: url, method: method, name: name
+  def compile(code)
+    nodes = @parser.parse(code)
+    puts nodes
+    url_method_node = nodes.url_method
+    method = HttpMethod.create(url_method_node.method.to_sym)
+    url = url_method_node.url
+    name = nodes.name_node.name
+    endpoint = Endpoint.new url: url, method: method, name: name
 
-      nodes.query_nodes.each do |query_node|
-        type = query_node.type.type
-        name = query_node.name.to_sym
-        endpoint.put_query_param(QueryParam.new name: name, type: type)
-      end
-
-      nodes.path_nodes.each do |path_nodes|
-        type = path_nodes.type.type
-        name = path_nodes.name.to_sym
-        endpoint.put_path_param(PathParam.new name: name, type: type)
-      end
-
-      nodes.header_nodes.each do |header_node|
-        endpoint.put_header(Header.new name: header_node.name, value: header_node.value)
-      end
-
-      endpoint
+    nodes.query_nodes.each do |query_node|
+      type = query_node.type.type
+      name = query_node.name.to_sym
+      endpoint.put_query_param(QueryParam.new name: name, type: type)
     end
+
+    nodes.path_nodes.each do |path_nodes|
+      type = path_nodes.type.type
+      name = path_nodes.name.to_sym
+      endpoint.put_path_param(PathParam.new name: name, type: type)
+    end
+
+    nodes.header_nodes.each do |header_node|
+      endpoint.put_header(Header.new name: header_node.name, value: header_node.value)
+    end
+    endpoint
   end
 
 end

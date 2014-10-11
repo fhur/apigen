722472a1a13576ed8527f522b47bc6794de14813
fhur <fernando@authy.com>
2014-10-11 11:04:51 -0500

    Created class apigen

---
+++ lib/apigen.rb
@@ -0,0 +1,20 @@
+require './lib/generation/generator.rb'
+require './lib/lang/apigen_compiler.rb'
+
+class Apigen
+
+  def initialize
+    @apigen_compiler = ApigenCompiler.new
+  end
+
+  # takes a program and a list of generators as input
+  # Uses the generator_writers to generate code for the given
+  # program and writes the generated code
+  def generate(program, generator_writers, opts={})
+    @apigen_compiler.compile program, opts
+    generator_writers.each do |g|
+      g.generate_and_write endpoint_group
+    end
+  end
+
+end

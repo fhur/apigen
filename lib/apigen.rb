fb21703c2ef153613b2ceb6e1078a9c54ee039f3
fhur <fernando@authy.com>
2014-10-12 19:32:04 -0500

    Created apigen_cli

--- lib/apigen.rb
+++ lib/apigen.rb
@@ -10,6 +10,8 @@ class Apigen
   # takes a program and a list of generators as input
   # Uses the generator_writers to generate code for the given
   # program and writes the generated code
+  # @param {string} program             The actual for for the program.
+  # @param {Array of GeneratorWriter}   An Array of generator writers
   def generate(program, generator_writers, opts={})
     @apigen_compiler.compile program, opts
     generator_writers.each do |g|
